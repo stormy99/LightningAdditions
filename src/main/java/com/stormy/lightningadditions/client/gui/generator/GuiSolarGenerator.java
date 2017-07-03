fbb4b5295df8fd39c466ea8cbe8c5249150f3064
MiningMark48 <miningmark48@gmail.com>
2017-07-03 03:12:04 -0400

    Copyrights

--- src/main/java/com/stormy/lightningadditions/client/gui/generator/GuiSolarGenerator.java
+++ src/main/java/com/stormy/lightningadditions/client/gui/generator/GuiSolarGenerator.java
@@ -1,3 +1,15 @@
+/*
+ *
+ *  * ********************************************************************************
+ *  * Copyright (c) 2017 StormyMode, MiningMark48. All Rights Reserved!
+ *  * This file is part of Lightning Additions (MC-Mod).
+ *  *
+ *  * This project cannot be copied and/or distributed without the express
+ *  * permission of StormyMode, MiningMark48 (Developers)!
+ *  * ********************************************************************************
+ *
+ */
+
 package com.stormy.lightningadditions.client.gui.generator;
 
 import com.stormy.lightningadditions.client.container.generator.ContainerSolarGenerator;
