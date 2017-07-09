ec843f3d7abff21b9fc9988977bb4b9aa2b07840
MiningMark48 <miningmark48@gmail.com>
2017-07-09 19:58:40 -0400

    Base tile entity class for generators

--- src/main/java/com/stormy/lightningadditions/tile/generator/TileEntitySolarGenerator.java
+++ src/main/java/com/stormy/lightningadditions/tile/generator/TileEntitySolarGenerator.java
@@ -12,30 +12,19 @@
 
 package com.stormy.lightningadditions.tile.generator;
 
-import cofh.api.energy.IEnergyProvider;
 import com.stormy.lightningadditions.block.generator.BlockSolarGenerator;
 import com.stormy.lightningadditions.init.ModBlocks;
 import com.stormy.lightningadditions.init.ModItems;
-import com.stormy.lightningadditions.tile.LATile;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.ITickable;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.energy.CapabilityEnergy;
-import net.minecraftforge.energy.IEnergyStorage;
 
-import javax.annotation.Nullable;
-
-public class TileEntitySolarGenerator extends LATile implements ITickable, IEnergyStorage, IInventory, IEnergyProvider {
+public class TileEntitySolarGenerator extends TileEntityBaseGenerator {
 
     public boolean isDay = true;
     public boolean canSeeSky = true;
@@ -47,152 +36,16 @@ public class TileEntitySolarGenerator extends LATile implements ITickable, IEner
     public int ipt_tach = 640;
     private int increase_per_tick;
 
-    private int maxRF = 150000;
+    private static int maxExtract = 1000;
+    private static int maxRF = 150000;
     private int current_RF;
     private int cooldown;
     public int cooldownMax = 120;
 
-    private int maxExtract = 1000;
-
-    public NonNullList<ItemStack> inventory;
-    private String customName;
-
     public TileEntitySolarGenerator(){
-        this.inventory = NonNullList.withSize(1, ItemStack.EMPTY);
+        super(NonNullList.withSize(1, ItemStack.EMPTY), maxExtract, maxRF);
     }
 
-    public String getCustomName(){
-        return customName;
-    }
-
-    public void setCustomName(String customName){
-        this.customName = customName;
-    }
-
-    @Override
-    public String getName() {
-        return customName;
-    }
-
-    @Override
-    public boolean hasCustomName() {
-        return this.customName != null && !this.customName.equals("");
-    }
-
-    @Nullable
-    @Override
-    public ITextComponent getDisplayName() {
-        return this.hasCustomName() ? new TextComponentString(this.customName) : new TextComponentString(world.getBlockState(pos).getBlock().getLocalizedName());
-    }
-
-    @Override
-    public int getSizeInventory() {
-        return this.inventory.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        for (ItemStack itemstack : this.inventory)
-        {
-            if (!itemstack.isEmpty())
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    @Nullable
-    @Override
-    public ItemStack getStackInSlot(int index) {
-        if (index < 0 || index >= this.getSizeInventory()){
-            return ItemStack.EMPTY;
-        }
-        return this.inventory.get(index);
-
-    }
-
-    @Nullable
-    @Override
-    public ItemStack decrStackSize(int index, int count) {
-        if (this.getStackInSlot(index) != ItemStack.EMPTY){
-            ItemStack itemStack;
-
-            if (this.getStackInSlot(index).getCount() <= count){
-                itemStack = this.getStackInSlot(index);
-                this.setInventorySlotContents(index, ItemStack.EMPTY);
-                this.markDirty();
-                return itemStack;
-            }else{
-                itemStack = this.getStackInSlot(index).splitStack(count);
-
-                if (this.getStackInSlot(index).getCount() <= 0){
-                    this.setInventorySlotContents(index, ItemStack.EMPTY);
-                }else{
-                    this.setInventorySlotContents(index, this.getStackInSlot(index));
-                }
-
-                this.markDirty();
-                return itemStack;
-
-            }
-        }else{
-            return ItemStack.EMPTY;
-        }
-    }
-
-    @Nullable
-    @Override
-    public ItemStack removeStackFromSlot(int index) {
-        ItemStack stack = this.getStackInSlot(index);
-        this.setInventorySlotContents(index, ItemStack.EMPTY);
-        return stack;
-    }
-
-    @Override
-    public void setInventorySlotContents(int index, @Nullable ItemStack stack) {
-        if (index < 0 || index >= this.getSizeInventory()){
-            return;
-        }
-
-        if (stack != ItemStack.EMPTY && stack.getCount() > this.getInventoryStackLimit()){
-            stack.setCount(this.getInventoryStackLimit());
-        }
-
-        if (stack != ItemStack.EMPTY && stack.isEmpty()){
-            stack = ItemStack.EMPTY;
-        }
-
-        this.inventory.set(index, stack);
-        this.markDirty();
-
-    }
-
-    @Override
-    public int getInventoryStackLimit() {
-        return 64;
-    }
-
-    @Override
-    public boolean isUsableByPlayer(EntityPlayer player) {
-        return this.world.getTileEntity(this.getPos()) == this && player.getDistanceSq(this.pos.add(0.5, 0.5, 0.5)) <= 64;
-    }
-
-    @Override
-    public void openInventory(EntityPlayer player) {
-
-    }
-
-    @Override
-    public void closeInventory(EntityPlayer player) {
-
-    }
-
-    @Override
-    public boolean isItemValidForSlot(int index, ItemStack stack) {
-        return true;
-    }
 
     @Override
     public int getField(int id) {
@@ -240,25 +93,9 @@ public class TileEntitySolarGenerator extends LATile implements ITickable, IEner
 
         super.writeToNBT(compound);
 
-        NBTTagList list = new NBTTagList();
-
-        for (int i = 0; i < this.getSizeInventory(); i++){
-            if (this.getStackInSlot(i) != ItemStack.EMPTY && this.getStackInSlot(i) != null){
-                NBTTagCompound stackTag = new NBTTagCompound();
-                stackTag.setByte("Slot", (byte) i);
-                this.getStackInSlot(i).writeToNBT(stackTag);
-                list.appendTag(stackTag);
-            }
-        }
-
-        compound.setTag("Items", list);
         compound.setInteger("currentRF", this.current_RF);
         compound.setInteger("cooldown", this.cooldown);
         compound.setInteger("ipt", this.increase_per_tick);
-
-        if (this.hasCustomName()){
-            compound.setString("CustomName", this.getCustomName());
-        }
 
         return compound;
 
@@ -267,19 +104,11 @@ public class TileEntitySolarGenerator extends LATile implements ITickable, IEner
     @Override
     public void readFromNBT(NBTTagCompound compound) {
         super.readFromNBT(compound);
-        NBTTagList list = compound.getTagList("Items", 10);
-        for (int i = 0; i < list.tagCount(); i++) {
-            NBTTagCompound stackTag = list.getCompoundTagAt(i);
-            int slot = stackTag.getByte("Slot") & 255;
-            this.setInventorySlotContents(slot, new ItemStack(stackTag));
-        }
+
         this.current_RF = compound.getInteger("currentRF");
         this.cooldown = compound.getInteger("cooldown");
         this.increase_per_tick = compound.getInteger("ipt");
 
-        if (compound.hasKey("CustomName", 8)) {
-            this.setCustomName(compound.getString("CustomName"));
-        }
     }
 
     @Override
@@ -382,7 +211,7 @@ public class TileEntitySolarGenerator extends LATile implements ITickable, IEner
         return false;
     }
 
-    private void pushEnergy(World world, BlockPos pos){
+    public void pushEnergy(World world, BlockPos pos){
         for (EnumFacing facing: EnumFacing.values()){
             TileEntity tile = world.getTileEntity(pos.offset(facing));
             if (tile != null){
