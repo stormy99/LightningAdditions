799a6f3a97aa0b3f070fe86c2991a6762b379f2b
MiningMark48 <miningmark48@gmail.com>
2017-07-10 13:34:47 -0400

    More Biofuels added to registry

--- src/main/java/com/stormy/lightningadditions/handler/generator/BioFuelRegistry.java
+++ src/main/java/com/stormy/lightningadditions/handler/generator/BioFuelRegistry.java
@@ -13,10 +13,9 @@
 package com.stormy.lightningadditions.handler.generator;
 
 import com.google.common.collect.Lists;
+import net.minecraft.block.BlockSapling;
 import net.minecraft.init.Items;
-import net.minecraft.item.ItemSeedFood;
-import net.minecraft.item.ItemSeeds;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -47,14 +46,34 @@ public class BioFuelRegistry {
 
         @Override
         public int getBurntime(ItemStack fuel) {
+
             if (fuel.isItemEqual(new ItemStack(Items.WHEAT))) {
                 return 60;
-            }else if (fuel.isItemEqual(new ItemStack(Items.CARROT))){
+            }else if (fuel.isItemEqual(new ItemStack(Items.LEATHER))){
+                return 140;
+            }else if (fuel.isItemEqual(new ItemStack(Items.ROTTEN_FLESH))){
+                return 40;
+            }else if (fuel.isItemEqual(new ItemStack(Items.RABBIT_HIDE))){
+                return 40;
+            }else if (fuel.isItemEqual(new ItemStack(Items.RABBIT_FOOT))){
+                return 160;
+            }else if (fuel.isItemEqual(new ItemStack(Items.NETHER_WART))){
                 return 120;
+            }else if (fuel.isItemEqual(new ItemStack(Items.SPIDER_EYE))){
+                return 80;
+            }else if (fuel.isItemEqual(new ItemStack(Items.SPECKLED_MELON))){
+                return 180;
+            }else if (fuel.isItemEqual(new ItemStack(Items.REEDS))){
+                return 40;
             }
 
-            if (fuel.getItem() instanceof ItemSeeds){
+
+            else if (fuel.getItem() instanceof ItemSeeds){
                 return 20;
+            }else if (fuel.getItem() instanceof ItemBlock && ((ItemBlock) fuel.getItem()).getBlock() instanceof BlockSapling){
+                return 20;
+            }else if (fuel.getItem() instanceof ItemFood){
+                return ((ItemFood) fuel.getItem()).getHealAmount(fuel) * 10;
             }
 
             return 0;
