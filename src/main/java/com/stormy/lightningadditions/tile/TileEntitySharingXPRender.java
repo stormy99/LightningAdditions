714dd0eb1c69f11c8acce78374bb4ce65b1a6eb5
StormyMode <stormymodding@gmail.com>
2017-07-16 19:33:43 +0100

    Sonic Screwdriver API (& Notation) \o/

--- src/main/java/com/stormy/lightningadditions/tile/TileEntitySharingXPRender.java
+++ src/main/java/com/stormy/lightningadditions/tile/TileEntitySharingXPRender.java
@@ -10,8 +10,7 @@
 
 package com.stormy.lightningadditions.tile;
 
-import org.lwjgl.opengl.GL11;
-
+import com.stormy.lightningadditions.init.ModItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
@@ -19,19 +18,27 @@ import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.client.ForgeHooksClient;
+import org.lwjgl.opengl.GL11;
 
 public class TileEntitySharingXPRender extends TileEntitySpecialRenderer<TileEntitySharingXP>
 {
-    private ItemStack emerald = new ItemStack(Items.EMERALD, 1);
+    private ItemStack tachyon = new ItemStack(ModItems.tachyon_shard, 1);
 
-    @Override
-    public void renderTileEntityAt(TileEntitySharingXP te, double x, double y, double z, float partialTicks, int destroyStage)
-    {
+    public void render(TileEntitySharingXP te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
+        ITextComponent levelsText = new TextComponentString("" + te.getStoredLevels());
+
+        if (te != null && te.getPos() != null && rendererDispatcher.cameraHitResult != null && rendererDispatcher.cameraHitResult.getBlockPos() != null && rendererDispatcher.cameraHitResult.getBlockPos().equals(te.getPos())) {
+            setLightmapDisabled(true);
+            drawNameplate(te, levelsText.getFormattedText(), x, y, z, 12);
+            setLightmapDisabled(false);
+        }
+
         double offset = Math.sin((te.getWorld().getTotalWorldTime() + partialTicks) / 8.0D) / 10.0D;
-        IBakedModel model = Minecraft.getMinecraft().getRenderItem().getItemModelWithOverrides(emerald, te.getWorld(), null);
+        IBakedModel model = Minecraft.getMinecraft().getRenderItem().getItemModelWithOverrides(tachyon, te.getWorld(), null);
 
         GlStateManager.enableRescaleNormal();
         GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
@@ -39,14 +46,13 @@ public class TileEntitySharingXPRender extends TileEntitySpecialRenderer<TileEnt
         RenderHelper.enableStandardItemLighting();
         GlStateManager.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
         GlStateManager.pushMatrix();
-        GlStateManager.translate(x + 0.5D, y + 0.4D + offset, z + 0.5D);
+        GlStateManager.translate(x + 0.5D, y + 2D + offset, z + 0.5D);
         GlStateManager.rotate((te.getWorld().getTotalWorldTime() + partialTicks) * 4.0F, 0.0F, 1.0F, 0.0F);
         model = ForgeHooksClient.handleCameraTransforms(model, TransformType.GROUND, false);
         Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-        Minecraft.getMinecraft().getRenderItem().renderItem(emerald, model);
+        Minecraft.getMinecraft().getRenderItem().renderItem(tachyon, model);
         GlStateManager.popMatrix();
         GlStateManager.disableRescaleNormal();
         GlStateManager.disableBlend();
-
     }
 }
