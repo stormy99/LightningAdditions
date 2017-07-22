9320ad76b41cadb6c8fc94ca007908f0f95ae571
MiningMark48 <miningmark48@gmail.com>
2017-07-22 00:28:57 -0400

    Fixed Tainted Soil model

--- src/main/java/com/stormy/lightningadditions/block/BlockCursedEarth.java
+++ src/main/java/com/stormy/lightningadditions/block/BlockCursedEarth.java
@@ -1,9 +1,9 @@
 package com.stormy.lightningadditions.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.util.EnumBlockRenderType;
@@ -26,6 +26,7 @@ public class BlockCursedEarth extends Block {
 
     public BlockCursedEarth() {
         super(Material.GRASS);
+        this.setSoundType(SoundType.GROUND);
         this.setTickRandomly(true);
         this.setHardness(0.5F);
         this.blockResistance = 200.0F;
@@ -48,8 +49,10 @@ public class BlockCursedEarth extends Block {
         return false;
     }
 
-    public boolean canSilkHarvest(World world, EntityPlayer player, int x, int y, int z, int metadata) {return true;}
-
+    @Override
+    protected boolean canSilkHarvest() {
+        return true;
+    }
 
     public Item getItemDropped(IBlockState state, Random rand, int fortune) {return Item.getItemFromBlock(Blocks.DIRT);}
 
