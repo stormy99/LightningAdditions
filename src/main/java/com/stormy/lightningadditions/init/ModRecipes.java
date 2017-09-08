6ee4d94e84a7cd17791d295b05c4daa702cc4d13
MiningMark48 <miningmark48@gmail.com>
2017-09-07 23:32:30 -0400

    Adding recipes :D

--- src/main/java/com/stormy/lightningadditions/init/ModRecipes.java
+++ src/main/java/com/stormy/lightningadditions/init/ModRecipes.java
@@ -4,6 +4,7 @@ import com.stormy.lightningadditions.utility.logger.LALogger;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
 
@@ -34,6 +35,18 @@ public class ModRecipes {
         GameRegistry.addRecipe(new ItemStack(ModItems.sponge_stick), "  x", " y ", "y  ", 'x', new ItemStack(ModBlocks.sponge), 'y', new ItemStack(Items.STICK, 1, OreDictionary.WILDCARD_VALUE));
 
         //Blocks
+        GameRegistry.addRecipe(new ItemStack(Blocks.GLASS), new ItemStack(ModBlocks.clear_glass), 0.05f);
+
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.breaker), "xxx", "xyz", "xxx", 'x', new ItemStack(Blocks.COBBLESTONE), 'y', new ItemStack(Items.REDSTONE), 'z', new ItemStack(Items.DIAMOND_PICKAXE));
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.placer), "xxx", "xyz", "xxx", 'x', new ItemStack(Blocks.COBBLESTONE), 'y', new ItemStack(Items.REDSTONE), 'z', new ItemStack(Items.REDSTONE));
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.igniter), "xxx", "xyz", "xxx", 'x', new ItemStack(Blocks.COBBLESTONE), 'y', new ItemStack(Items.REDSTONE), 'z', new ItemStack(Items.FLINT_AND_STEEL));
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.compressed_bookshelf), "xxx", "zyz", "xxx", 'x', new ItemStack(Blocks.BOOKSHELF), 'y', new ItemStack(Items.BOOK), 'z', new ItemStack(Items.GOLD_INGOT));
+        GameRegistry.addRecipe(new ItemStack(ModBlockContainers.ender_hopper), "xxx", "zyz", " a ", 'x', new ItemStack(Items.IRON_INGOT), 'y', new ItemStack(Blocks.HOPPER), 'z', new ItemStack(Items.ENDER_EYE), 'a', new ItemStack(ModItems.tachyon_shard));
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.magnetized_chest), "xxx", "yzy", "xxx", 'x', new ItemStack(Blocks.COBBLESTONE), 'y', new ItemStack(Blocks.CHEST), 'z', new ItemStack(ModItems.tachyon_shard));
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.reinforced_obsidian, 4), "xyx", "yxy", "xyx", 'x', new ItemStack(Blocks.OBSIDIAN), 'y', new ItemStack(Blocks.IRON_BARS));
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.reinforced_obsidianglass, 4), "xyx", "yxy", "xyx", 'x', new ItemStack(ModBlocks.reinforced_obsidian), 'y', new ItemStack(Blocks.GLASS));
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.share_xp), "x x", "yzy", "aaa", 'x', new ItemStack(Items.ENDER_EYE), 'y', new ItemStack(Items.GLOWSTONE_DUST), 'z', new ItemStack(ModItems.tachyon_shard), 'a', new ItemStack(Blocks.OBSIDIAN));
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.void_block), "xxx", "zyz", "xxx", 'x', new ItemStack(Blocks.WOOL));
         GameRegistry.addRecipe(new ItemStack(ModBlocks.particle_accellerator), "xyx", "yzy", "aba", 'x', new ItemStack(Items.DIAMOND), 'y', new ItemStack(Items.IRON_INGOT), 'z', new ItemStack(Items.GLOWSTONE_DUST), 'a', new ItemStack(Items.GOLD_INGOT), 'b', new ItemStack(Items.REDSTONE));
 
         LALogger.log("LA Recipes registered.");
