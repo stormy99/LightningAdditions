14fdc2373a6265fd12e32f6f636f351297ef8f44
MiningMark48 <miningmark48@gmail.com>
2017-09-14 18:37:59 -0400

    Main mod copyrights added, have yet to do LightningLib

--- src/main/java/com/stormy/lightningadditions/utility/inventory/BlockHelper.java
+++ src/main/java/com/stormy/lightningadditions/utility/inventory/BlockHelper.java
@@ -1,3 +1,15 @@
+/*
+ *
+ *  * ********************************************************************************
+ *  * Copyright (c) 2017 StormyMode, MiningMark48. All Rights Reserved!
+ *  * This file is part of Lightning Additions (MC-Mod).
+ *  *
+ *  * This project cannot be copied and/or distributed without the express
+ *  * permission of StormyMode, MiningMark48 (Developers)!
+ *  * ********************************************************************************
+ *
+ */
+
 package com.stormy.lightningadditions.utility.inventory;
 
 import net.minecraft.block.Block;
