a4c5b608e4685db51e0585d2e2e1a09296b1e9c5
StormyMode <stormymodding@gmail.com>
2019-02-17 19:45:02 +0000

    Initial Fabric Load

--- src/main/java/com/stormy/lightningadditions/LightningAdditions.java
+++ src/main/java/com/stormy/lightningadditions/LightningAdditions.java
@@ -1,14 +1,33 @@
 package com.stormy.lightningadditions;
 
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.crash.CrashException;
+import net.minecraft.util.crash.CrashReport;
 
 public class LightningAdditions implements ModInitializer {
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		System.out.println("Hello, this is LightningAdditions here!");
+		System.out.println("[Lightning Additions!]: Initialised");
 	}
+
+	public static MinecraftServer getServer() {
+		if(FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT){
+			return MinecraftClient.getInstance().getServer();
+		} else {
+			return (MinecraftServer) FabricLoader.getInstance().getGameInstance();
+		}
+	}
+
+	public static void crashReport(String message, Throwable throwable)
+	{ throw new CrashException(new CrashReport(message, throwable)); }
+
 }
--- src/main/java/com/stormy/lightningadditions/reference/ModReferences.java
+++ src/main/java/com/stormy/lightningadditions/reference/ModReferences.java
@@ -1,9 +1,14 @@
-package com.stormy.lightningadditions;
+package com.stormy.lightningadditions.reference;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ModReferences
 {
     public static final String MODID = "lightningadditions";
     public static final String MODNAME = "Lightning Additions";
     public static final String VERSION = "0.0.3";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+
 
 }
